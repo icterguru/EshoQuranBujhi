9bc077108b76a1e8c1b6dce95acc0ab0d6e28f0d
ICTER Guru <icterguru@gmail.com>
2018-03-10 08:44:41 -0500

    Surah Al-Asr program and ppt included

---
+++ src/main/java/alQuran/SurahAlAsr.java
@@ -0,0 +1,18 @@
+package alQuran;
+
+public class SurahAlAsr {
+
+	public static void main(String[] args) {
+		
+		boolean hasIman = true;
+		boolean hasGoodDeed = true;
+		boolean urgeForTruth= true;
+		boolean askForPatience= true;
+		
+		boolean isSucceeded = hasIman && hasGoodDeed && urgeForTruth && askForPatience;
+
+		System.out.println("isSucceeded = " + isSucceeded);
+		
+	}
+
+}
